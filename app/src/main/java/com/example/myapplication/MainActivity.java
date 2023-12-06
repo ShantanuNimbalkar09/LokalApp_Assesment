07d7d0c88ff77a6f3bdd108bde1508a6720c2e45
ShantanuNimbalkar09 <nimbalkar.github@gmail.com>
2023-12-06 23:12:45 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,277 @@
+package com.example.myapplication;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.squareup.picasso.Picasso;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import model.Product;
+
+public class MainActivity extends AppCompatActivity {
+    RecyclerView recyclerView;
+    List<Product> productList;
+
+    List<Product> filteredProductList;
+
+    Spinner categoryLIst;
+
+    String URL = "https://dummyjson.com/products";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        productList = new ArrayList<>();
+        recyclerView = findViewById(R.id.productList);
+        recyclerView.setNestedScrollingEnabled(true);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        categoryLIst = findViewById(R.id.category_List);
+
+        filteredProductList = new ArrayList<>();
+
+
+        ProductAdapter adapter = new ProductAdapter(productList);
+        recyclerView.setAdapter(adapter);
+
+        categoryLIst.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String selectedCategory = parent.getItemAtPosition(position).toString();
+
+
+                updateFilteredProductList(selectedCategory);
+
+                updateRecyclerView(filteredProductList);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                Toast.makeText(getApplicationContext(),"Nothing is selected",Toast.LENGTH_LONG).show();
+            }
+        });
+
+
+        fetchProductData();
+    }
+
+
+    void fetchProductData() {
+
+        RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
+
+
+        StringRequest request = new StringRequest(Request.Method.GET, URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Log.d("VolleyResponse", "Success: " + response);
+
+
+                JSONObject jsonResponse = null;
+                try {
+                    jsonResponse = new JSONObject(response);
+                } catch (JSONException e) {
+                    throw new RuntimeException(e);
+                }
+
+
+                try {
+                    parseJson(jsonResponse);
+                } catch (JSONException e) {
+                    throw new RuntimeException(e);
+                }
+                updateRecyclerView(productList);
+                updateCategoryList();
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+
+
+        queue.add(request);
+    }
+
+
+    void parseJson(JSONObject response) throws JSONException {
+        try {
+            JSONArray jsonArray = response.getJSONArray("products");
+
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject objectResponse = jsonArray.getJSONObject(i);
+                Product product = new Product();
+                product.setId(objectResponse.getInt("id"));
+                product.setTitle(objectResponse.getString("title"));
+                product.setDescription(objectResponse.getString("description"));
+                product.setPrice(objectResponse.getInt("price"));
+                product.setThumbnail(objectResponse.getString("thumbnail"));
+                product.setRating(objectResponse.getDouble("rating"));
+                product.setBrand(objectResponse.getString("brand"));
+                product.setCategory(objectResponse.getString("category"));
+                JSONArray imagesarray = objectResponse.getJSONArray("images");
+                List<String> imageList = new ArrayList<>();
+                for (int j = 0; j < imagesarray.length(); j++) {
+                    String imageUrl = imagesarray.getString(j);
+                    imageList.add(imageUrl);
+                }
+                product.setImages(imageList);
+
+                productList.add(product);
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    private void updateRecyclerView(List<Product> productList) {
+        ProductAdapter adapter = new ProductAdapter(productList);
+        recyclerView.setAdapter(adapter);
+    }
+
+    class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ViewHolder> {
+
+        private List<Product> productList;
+
+
+        public ProductAdapter(List<Product> productList) {
+            this.productList = productList;
+
+        }
+
+
+//        public void setProductList(List<Product> productList) {
+//            this.productList = productList;
+//            notifyDataSetChanged();
+//        }
+
+
+        @NonNull
+        @Override
+        public ProductAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_layout, parent, false);
+            return new ViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull ProductAdapter.ViewHolder holder, @SuppressLint("RecyclerView") int position) {
+
+            Product product = productList.get(position);
+            holder.title.setText(product.getTitle());
+            holder.description.setText(product.getDescription());
+            String price1="Rs."+String.valueOf(product.getPrice());
+            holder.price.setText(price1);
+            holder.rating.setText(String.valueOf(product.getRating()));
+
+            Picasso.get().load(product.getThumbnail()).into(holder.thumbnail);
+
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return productList.size();
+        }
+
+        class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+            TextView title, description, rating, price;
+            ImageView thumbnail;
+
+
+            public ViewHolder(@NonNull View itemView) {
+                super(itemView);
+                title = itemView.findViewById(R.id.title);
+                description = itemView.findViewById(R.id.description);
+                rating = itemView.findViewById(R.id.rating);
+                price = itemView.findViewById(R.id.price);
+
+                thumbnail = itemView.findViewById(R.id.thumbnailImageView);
+
+                itemView.setOnClickListener(this);
+            }
+
+
+            @Override
+            public void onClick(View v) {
+                int position = this.getAdapterPosition();
+                Product product = productList.get(position);
+
+                openProductDetailsActivity(product);
+            }
+        }
+    }
+
+    void openProductDetailsActivity(Product product) {
+        Intent intent = new Intent(MainActivity.this, Product_Details_Activity.class);
+        intent.putExtra("product", product);
+        startActivity(intent);
+
+    }
+
+    public void updateCategoryList() {
+        Set<String> Product_category = new HashSet<>();
+        for (Product product : productList) {
+            Log.d("ProductList", "Category: " + String.valueOf(product.getCategory()));
+            Product_category.add(product.getCategory());
+        }
+        Log.d("categories", String.valueOf(Product_category));
+        List<String> catergories = new ArrayList<>(Product_category);
+
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
+                this, com.google.android.material.R.layout.support_simple_spinner_dropdown_item, catergories);
+        arrayAdapter.setDropDownViewResource(com.google.android.material.R.layout.support_simple_spinner_dropdown_item);
+        categoryLIst.setAdapter(arrayAdapter);
+    }
+
+
+    private void updateFilteredProductList(String selectedCategory) {
+        filteredProductList.clear();
+        for (Product product : productList) {
+            if (product.getCategory().equals(selectedCategory)) {
+                filteredProductList.add(product);
+            }
+        }
+    }
+
+
+}
---
+++ app/src/main/java/com/example/myapplication/Product_Details_Activity.java
@@ -0,0 +1,141 @@
+package com.example.myapplication;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager2.widget.ViewPager2;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageSwitcher;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Target;
+
+import java.util.List;
+
+import model.Product;
+
+public class Product_Details_Activity extends AppCompatActivity {
+    ImageView images,leftarrow,rightarrow;
+    TextView title, description, brand, rating, price;
+
+    ViewPager2 viewpager;
+    ImageSliderAdapter imageSliderAdapter;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product_details);
+
+        images = findViewById(R.id.images);
+        title = findViewById(R.id.title);
+        description = findViewById(R.id.description);
+        brand = findViewById(R.id.Brand);
+        rating = findViewById(R.id.rating);
+        price = findViewById(R.id.Price);
+        viewpager=findViewById(R.id.viewPager);
+        leftarrow=findViewById(R.id.arrowLeft);
+        rightarrow=findViewById(R.id.arrowRight);
+
+
+        Intent intent = getIntent();
+
+        if (intent != null && intent.hasExtra("product")) {
+            Product product = (Product) intent.getSerializableExtra("product");
+
+            // Now you can use the product details as needed
+            Picasso.get().load(product.getThumbnail()).into(images);
+
+            title.setText(product.getTitle());
+            description.setText(product.getDescription());
+
+            String company = "Company: " + product.getBrand();
+            brand.setText(company);
+
+            String stars = "Stars: " + String.valueOf(product.getRating());
+            rating.setText(stars);
+
+            String money="Rs."+String.valueOf(product.getPrice());
+            price.setText(money);
+
+
+            Log.d("images", String.valueOf(product.getImages()));
+
+            imageSliderAdapter=new ImageSliderAdapter(product.getImages());
+            viewpager.setAdapter(imageSliderAdapter);
+
+
+            leftarrow.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int currentItem = viewpager.getCurrentItem();
+                    if (currentItem > 0) {
+                        viewpager.setCurrentItem(currentItem - 1);
+                    }
+                }
+            });
+
+            rightarrow.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int currentItem = viewpager.getCurrentItem();
+                    if (currentItem < imageSliderAdapter.getItemCount() - 1) {
+                        viewpager.setCurrentItem(currentItem + 1);
+                    }
+                }
+            });
+
+
+
+
+
+        }
+
+
+    }
+
+    public class ImageSliderAdapter extends RecyclerView.Adapter<ImageSliderAdapter.ViewHolder> {
+
+        private List<String> imageUrls;
+
+        public ImageSliderAdapter(List<String> imageUrls) {
+            this.imageUrls = imageUrls;
+        }
+        @NonNull
+        @Override
+        public ImageSliderAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.image_slider, parent, false);
+            return new ViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull ImageSliderAdapter.ViewHolder holder, int position) {
+            String imageUrl = imageUrls.get(position);
+            Picasso.get().load(imageUrl).into(holder.imageView);
+        }
+
+        @Override
+        public int getItemCount() {
+            return imageUrls.size();
+        }
+
+        class ViewHolder extends RecyclerView.ViewHolder {
+            ImageView imageView;
+
+            public ViewHolder(@NonNull View itemView) {
+                super(itemView);
+                imageView = itemView.findViewById(R.id.sliderImage);
+            }
+        }
+    }
+}
